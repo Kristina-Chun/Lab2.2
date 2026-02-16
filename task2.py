600e810e50f4db3f7c619475e2682ad8dc97de7a
Kristina-Chun <158212661+Kristina-Chun@users.noreply.github.com>
2026-02-16 22:25:53 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,72 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+    """Класс, представляющий книгу."""
+    def __init__(self, id_, name, pages):
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f"Book(id_={self.id}, name={repr(self.name)}, pages={self.pages})"
+
+
+class Library:
+    """Класс, представляющий библиотеку."""
+    def __init__(self, books=None):
+        """
+        Инициализирует библиотеку с заданным списком книг.
+        Если список не передан, библиотека будет пустой.
+        """
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+
+    def get_next_book_id(self):
+        """
+        Возвращает следующий доступный идентификатор для новой книги.
+        Если библиотека пуста, возвращает 1.
+        Иначе возвращает id последней книги + 1.
+        """
+        if not self.books:
+            return 1
+        return self.books[-1].id + 1
+
+    def get_index_by_book_id(self, book_id):
+        """
+        Возвращает индекс книги в списке по её id.
+        Если книга не найдена, вызывает ValueError.
+        """
+        for index, book in enumerate(self.books):
+            if book.id == book_id:
+                return index
+        raise ValueError("Книги с запрашиваемым id не существует")
+
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
